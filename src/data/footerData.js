fc73cdcd2d811d8e38257261ed8a32698b44435d
Mrdheerajcs <mrdheerajtech@gmail.com>
2026-01-30 23:35:21 +0530

    change some thing

---
+++ src/data/footerData.js
@@ -0,0 +1,45 @@
+export const footerData = {
+    company: {
+        name: "Auspicious Coders",
+        acronym: "AC",
+        description:
+            "Transforming businesses through innovative digital solutions. We build the future, one line of code at a time.",
+        socialLinks: [
+            { name: "Facebook", url: "#", icon: "Facebook" },
+            { name: "Twitter", url: "#", icon: "Twitter" },
+            { name: "Linkedin", url: "#", icon: "Linkedin" },
+            { name: "Github", url: "#", icon: "Github" },
+        ],
+    },
+    quickLinks: [
+        "Home",
+        "Services",
+        "Projects",
+        "About Us",
+        "Team",
+        "Contact",
+    ],
+    services: [
+        "Web Development",
+        "Mobile Apps",
+        "UI/UX Design",
+        "Cloud DevOps",
+        "AI/ML",
+        "Consulting",
+    ],
+    contact: {
+        address: "123 Tech Street, Silicon Valley\nCalifornia, USA",
+        phone: [
+            "+91 9472971255",
+            "+91 7000859656",
+            "+91 955331296"
+        ],
+        email: "auspicious.coder.pvt.ltd@gmail.com",
+    }
+    ,
+    policies: [
+        { name: "Privacy Policy", url: "#" },
+        { name: "Terms of Service", url: "#" },
+        { name: "Cookie Policy", url: "#" },
+    ],
+};
